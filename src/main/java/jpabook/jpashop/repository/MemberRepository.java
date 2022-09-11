fd79940ffe18f0306499a55882e7ad5201fe04bf
gohardbro <wkd9155@gmail.com>
2022-09-11 21:53:06 +0900

    #JPA활용1 #상품도메인 완

--- src/main/java/jpabook/jpashop/repository/MemberRepository.java
+++ src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -9,7 +9,7 @@ import javax.persistence.PersistenceContext;
 import java.util.List;
 
 @Repository
-@RequiredArgsConstructor
+@RequiredArgsConstructor // final 이나 @NotNull 이 붙은 필드의 생성자를 자동 생성해주는 롬복 어노테이션
 public class MemberRepository {
 
 //    @PersistenceContext
