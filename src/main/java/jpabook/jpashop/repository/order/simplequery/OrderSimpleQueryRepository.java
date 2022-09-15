0633a7426deeb3a2fc5e764868c8b0d79d0ab3a2
gohardbro <wkd9155@gmail.com>
2022-09-15 16:45:40 +0900

    :zap: #API 개발고급 #지연로딩과 조회성능 최적화

---
+++ src/main/java/jpabook/jpashop/repository/order/simplequery/OrderSimpleQueryRepository.java
@@ -0,0 +1,24 @@
+package jpabook.jpashop.repository.order.simplequery;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import java.util.List;
+
+@Repository
+@RequiredArgsConstructor
+public class OrderSimpleQueryRepository {
+
+    private final EntityManager em;
+
+    //재사용성이 낮다 , 쿼리 맞춤 DTO 를 만들었기 때문에 논리적관점에서 repository 에 맞지 않음 그래서 따로 분리시킴
+    public List<OrderSimpleQueryDto> findOrderDtos() {
+        return em.createQuery(
+                        "select new jpabook.jpashop.repository.order.simplequery.OrderSimpleQueryDto(o.id, m.name, o.orderDate, o.status, d.address)" +
+                                " from Order o" +
+                                " join o.member m" +
+                                " join o.delivery d", OrderSimpleQueryDto.class)
+                .getResultList();
+    }
+}
